ac8d3279830dd33f43c0a45f0173894871ba51fa
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-05 22:27:18 +0200

    ADD hotel carousel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_ui_starter/widgets/destination_carousel.dart';
+import 'package:flutter_travel_ui_starter/widgets/hotel_carousel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -58,7 +59,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
             ),
-            // SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: _icons
@@ -73,6 +73,10 @@ class _HomeScreenState extends State<HomeScreen> {
               height: 20,
             ),
             DestinationCarousel(),
+            SizedBox(
+              height: 20,
+            ),
+            HoteCarousel(),
           ],
         ),
       ),
